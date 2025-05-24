d201e468564fe3671cd20df9365e8964122e643a
Juampy <coriajuampycj@gmail.com>
2025-05-23 23:16:48 -0300

    fin

--- 11 Recursividad/CORIA_TP-11.py
+++ 11 Recursividad/CORIA_TP-11.py
@@ -80,7 +80,7 @@ print(binario_rec(10))  # "1010"
 
 
 
-#Implementá una función recursiva llamada es_palindromo(palabra) que reciba una
+#5) Implementá una función recursiva llamada es_palindromo(palabra) que reciba una
 #cadena de texto sin espacios ni tildes, y devuelva True si es un palíndromo o False si no lo es.
 # Requisitos:
 #La solución debe ser recursiva.
@@ -99,3 +99,70 @@ def es_palindromo(palabra):
 palabra = es_palindromo(input("Insertar palabra sin espacios ni tildes: "))
 
 print(palabra)
+
+
+
+#6) Escribí una función recursiva en Python llamada suma_digitos(n) que reciba un
+#número entero positivo y devuelva la suma de todos sus dígitos.
+#3 Restricciones:
+#No se puede convertir el número a string.
+#Usá operaciones matemáticas (%, //) y recursión.
+#Ejemplos:
+#suma_digitos(1234) → 10 (1 + 2 + 3 + 4)
+#suma_digitos(9) → 9
+#suma_digitos(305) → 8 (3 + 0 + 5)
+
+
+def suma_digitos(n):
+    if n < 10:
+        return 0
+    else:
+        return (n % 10) + suma_digitos(n // 10)
+
+
+print(suma_digitos(555))
+
+
+
+#7) Un niño está construyendo una pirámide con bloques. En el nivel más bajo coloca n
+#bloques, en el siguiente nivel uno menos (n - 1), y así sucesivamente hasta llegar al
+#último nivel con un solo bloque.
+#Escribí una función recursiva contar_bloques(n) que reciba el número de bloques en el
+#nivel más bajo y devuelva el total de bloques que necesita para construir toda la
+#pirámide.
+# Ejemplos:
+#contar_bloques(1) → 1 (1)
+#contar_bloques(2) → 3 (2 + 1)
+#contar_bloques(4) → 10 (4 + 3 + 2 + 1)
+
+def contar_bloques(n):
+    if n == 1:
+        return 1
+    else:
+        return n + contar_bloques(n - 1)
+
+print(contar_bloques(4))
+
+
+
+#8) Escribí una función recursiva llamada contar_digito(numero, digito) que reciba un
+#número entero positivo (numero) y un dígito (entre 0 y 9), y devuelva cuántas veces
+#aparece ese dígito dentro del número.
+# Ejemplos:
+#contar_digito(12233421, 2) → 3
+#contar_digito(5555, 5) → 4 
+#contar_digito(123456, 7) → 0
+
+
+def contar_digito(numero, digito):
+    if numero == 0:
+        return 0
+    else:
+        if numero % 10 == digito:
+            return 1 + contar_digito(numero // 10, digito)
+        else:
+            return contar_digito(numero // 10, digito)
+
+         
+print(contar_digito(232,2))
+
